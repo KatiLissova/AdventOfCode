5cc93b1c85fdf5b24df7432d1d433daa90f7af74
Katerina Lissova <kati.lissova@gmail.com>
2023-12-09 13:45:17 +0100

    Tag 8 und 9

---
+++ Tag8/Program.cs
@@ -0,0 +1,54 @@
+﻿/*List<int> liste = new List<int>();
+
+liste.Add(5);
+liste.Add(10);
+liste.Add(55);
+
+Console.WriteLine(liste[1]);
+Console.WriteLine(liste.IndexOf(55));
+
+string a = "Hallo";
+Console.WriteLine(a.Substring(2, 1));
+Console.WriteLine(a.Substring(1, 3));
+
+Console.WriteLine("Ende");
+*/
+
+string a = "Katerina 456 Papa";
+string[] b;
+b = a.Split(' ');
+
+string[] res = a.Split(':');
+string data = res[1];
+string gewinnnummer = data.Split('|')[0];
+string meinenummer = data.Split('|')[1];
+string[] gew = gewinnnummer.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+string[] mei = meinenummer.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+string[] zeilen;
+zeilen = File.ReadAllLines("input.txt");
+string weg = zeilen[0];
+
+string position = "AAA";
+int züge = 0;
+
+int num = 0;
+while (position!= "ZZZ"){
+    int platz = 2;
+    while(zeilen[platz].Substring(0, 3)!= position) {
+        platz = platz + 1;
+    }
+    if(weg.Length == num) {
+        num = 0;
+    }
+    char richtung=weg[num];
+    if (richtung=='L') {
+        position = (zeilen[platz].Substring(7, 3));
+    } else {
+        position = (zeilen[platz].Substring(12, 3));
+    }
+    züge = züge+1;
+    num = num + 1;
+}
+
+Console.WriteLine(züge);
---
+++ Tag9/Program.cs
@@ -0,0 +1,57 @@
+﻿string[] zeilen;
+int ergebnis = 0;
+int i = 0;
+zeilen = File.ReadAllLines("input.txt");
+
+/*for (int k = 0; k < 10; k = k+1) {
+    Console.WriteLine(k);
+}
+
+int k = 0;
+while (k < 10) {
+    Console.WriteLine(k);
+    k = k+1;
+}*/
+
+while(i<zeilen.Length){
+    string data;
+    data = zeilen[i];
+    string[] teil;
+    teil = data.Split(' ');
+
+    int[, ] zahlen = new int[teil.Length, teil.Length];
+
+    for (int j = 0; j < teil.Length; j++)
+    {
+        zahlen[0, j] = int.Parse(teil[j]);
+    }
+
+
+    for (int r = 1; r < teil.Length; r++)
+    {
+        int colAnzahl = teil.Length - r;
+        for (int c = 0; c < colAnzahl; c++)
+        {
+            zahlen[r, c] = zahlen[r-1, c+1]-zahlen[r-1, c];
+        }
+    }
+    int summe = 0;
+    for (int r = 0; r < teil.Length; r++)
+    {
+        int c = teil.Length-1 - r;
+        summe = summe + zahlen[r, c];
+    }
+    //ergebnis = ergebnis+summe;
+    //Console.WriteLine(summe);
+
+    int links = 0;
+    for (int r = teil.Length-1; r >= 0; r--)
+    {
+        links = zahlen[r, 0] - links;
+    }
+    ergebnis = ergebnis+links;
+
+    i = i+1;
+}
+ 
+ Console.WriteLine(ergebnis);
